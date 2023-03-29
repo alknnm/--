f21bc2b3590640ad268791e2f7046d5b2da3b256
alknnm <azat173@yandex.ru>
2023-03-29 20:51:44 +0400

    js first

---
+++ script.js
@@ -0,0 +1,18 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько вы оцените его?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько вы оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
